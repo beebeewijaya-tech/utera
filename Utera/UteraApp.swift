007c9372fe3828e0a56e554fc45ce95a074227b4
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-26 20:06:30 +0700

    Initial Commit

---
+++ Utera/UteraApp.swift
@@ -0,0 +1,17 @@
+//
+//  UteraApp.swift
+//  Utera
+//
+//  Created by Bee Wijaya on 26/06/26.
+//
+
+import SwiftUI
+
+@main
+struct UteraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
