b761db31106fd253ea36923168e3ca741a5ff876
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-27 01:55:01 +0700

    feat: implementing cycle form screen
    -- What
    
    add the screen for the cycle form, containing several form field on the page
    
    update the page using several reusable component to be a lego block
    
    -- Why
    
    this approach is using component based style, not componetize everything just reusable one
    
    goal is to treat as lego

---
+++ Utera/Component/AppPill/AppPill.swift
@@ -0,0 +1,50 @@
+//
+//  AppPill.swift
+//  Utera
+//
+//  Created by Bee Wijaya on 27/06/26.
+//
+
+import SwiftUI
+
+enum AppPillStyle {
+    case active, inactive
+    
+    var foreground: Color {
+        switch self {
+        case .active:
+            return Color("Primary")
+        case .inactive:
+            return Color("TextSecondary")
+        }
+    }
+}
+
+struct AppPill: View {
+    var label: String
+    var style: AppPillStyle
+    var action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            VStack {
+                Text(label)
+                    .foregroundStyle(style.foreground)
+            }
+            .padding()
+            .background(.white)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(style.foreground, lineWidth: 1)
+            }
+        }
+    }
+}
+
+#Preview {
+    AppPill(label: "Pretty Regular", style: .active) {
+        
+    }
+}
