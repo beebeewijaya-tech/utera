a6efeef35eb2be0107d088a94e22f8e0310474bf
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-27 13:37:26 +0700

    feat: add newRelic observability tools
    -- What
    
    goal is to enabling the monitoring of the app including session replay and the crash analysis

---
+++ Utera/Screen/Home/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  Utera
+//
+//  Created by Bee Wijaya on 27/06/26.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
