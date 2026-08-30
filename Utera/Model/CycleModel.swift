1ff156fb5bee6f363603f9af0c8de83c5007cea0
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-08-30 23:57:08 +0700

    feat(calendar): implementing fertility & period logic
    implement calendar calculation by using map and adding value of the day, also doing difference by using datecomponents from & to
    the goal is to get the fertility window and the ovulation day also the next period window
    
    all of these will be rendered throughout the calendar with a different UI from the calendar day pill

--- Utera/Model/CycleModel.swift
+++ Utera/Model/CycleModel.swift
@@ -15,18 +15,21 @@ class CycleModel {
     var avgPeriod: Int
     var cycleRegular: String
     var trackingGoal: String
+    var createdAt: Date
     
     init(
         date: Date,
         avgCycle: Int,
         avgPeriod: Int,
         cycleRegular: String,
-        trackingGoal: String
+        trackingGoal: String,
+        createdAt: Date = .now
     ) {
         self.date = date
         self.avgCycle = avgCycle
         self.avgPeriod = avgPeriod
         self.cycleRegular = cycleRegular
         self.trackingGoal = trackingGoal
+        self.createdAt = createdAt
     }
 }
